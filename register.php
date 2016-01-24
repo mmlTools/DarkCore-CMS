41226a524eedfb35a6e66ea2d5056238c38d2605
darksoke <darksoke@users.noreply.github.com>
2016-01-24 23:05:23 +0200

    Update register.php
    Fixed style

--- register.php
+++ register.php
@@ -13,10 +13,9 @@
 	</div>
 	<div id='content'>
 		<div id='content-wrapper'>
-			<div class='arm-title'>REGISTER NEW ACCOUNT</div>
 			<div id='user-box'>
 				<div class='reg-alts-part'>
-					<h1 class='user-box-title'>New-Account Informations</h1>
+					<h3 class='user-box-title'>Register new account</h3>
 					<span class='box-divider'></span>
 					<form action="core/do_register.php" method="post" enctype="multipart/form-data">
 						<input class='reg-input' type='text' 		value='' name='Username'		autocomplete='off' placeholder='Username**' required>
@@ -37,24 +36,13 @@
 						?>
 						<input id='user-submit' type='submit' name='regsubmit' 	value='Register new Account'>
 					</form>
-					<span class='box-divider'></span>
 					<span class='reg-mark'>All (**) marked fields are required !</span>
 				</div>
 				<div class='reg-left-block'>
 				<div class='reg-alts-part-sec'>
-						<h1 class='user-box-title'>Few reassons why you should choose us</h1>
+						<h3 class='user-box-title'>Few reassons why you should choose us</h3>
 						<span class='box-divider'></span>
 						<p style="color:#009966">We are a great and friendly community started back in 17/07/2014 , our server is still under development and we are trying to make the gameplay as fun as possible. We love our great community and we listen every single suggestion you'll give us. Server is PvE based but we are trying to listen to your PvP suggestions and doing our best to bring you more PvP content. We bring you great solo PvE experience and few group world bosses. We have so far 18 tier sets,6 traveler tiers,upgradable VIP items. We have tons of custom quests and over 15000 custom items and numbers are still growing almost every week. We managed to fix a lot of common bugs and edited character creations. That means you won't start with useless stuff in your bags.</p> 
-					</div>
-					<div class='reg-alts-part-sec'>
-						<h1 class='user-box-title'>Players feedback</h1>
-						<span class='box-divider'></span>
-						<table>
-						<tr>
-						<td style='width:100px;color:#009999'>Darksoke: </td>
-						<td style='color:#009966'>This is just a test feedback until the website will be up on the host.</td> 
-						</tr>
-						</table>
 					</div>
 				</div>
 			</div>
